3e79febb2a8a74ab870ae9b1020fdb694a3a2be1
Rock Whitney <rock@mach50.com>
2023-04-16 10:54:16 -0600

    change mach50

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [-122.31, 47.60],
+            center: [-132.31, 57.60],
             container: 'map',
             map: map,
             zoom: 12
